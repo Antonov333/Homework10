fbb69346c69e828babba6db8fd956eee2a772381
Antonov333 <antonovsergei333@yahoo.com>
2023-03-13 14:27:02 +0300

    Just started with homework 10

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("\nHi!\n\nJust started with homework 10");
     }
 }
